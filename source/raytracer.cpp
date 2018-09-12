813be8a5cbdbf2ca3ed08c5e571126df77c251a2
David Fries <david.fries@uni-weimar.de>
2018-09-12 19:44:50 +0200

    added animation loop, still commented

--- source/raytracer.cpp
+++ source/raytracer.cpp
@@ -6,12 +6,27 @@
 
 int main(int argc, char* argv[])
 {
-  unsigned const width = 600;
-  unsigned const height = 600;
+  unsigned const width = 800;
+  unsigned const height = 800;
+  /*
+  for (int i = 1; i <= 120; ++i)
+  {
+  std::string const filename = "./animation"+i+".ppm";
+
+  Sdf_loader loader;
+  Scene scene = loader.load_scene("/home/david/Git/raytracer/programmiersprachen-raytracer/animation"+i+".txt");
+
+  Renderer app(width, height, filename, scene);
+  app.render();
+
+  std::thread thr([&app]() { app.render(); });
+
+  }
+*/
   std::string const filename = "./checkerboard.ppm";
 
   Sdf_loader loader;
-  Scene scene = loader.load_scene("/home/IN/xavo6170/git/programmiersprachen-raytracer/testload.txt");
+  Scene scene = loader.load_scene("/home/david/Git/raytracer/programmiersprachen-raytracer/testload.txt");
 
   Renderer app(width, height, filename, scene);
   app.render();
